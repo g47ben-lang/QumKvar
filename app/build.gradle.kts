9691fcdf18150d63206bcfe10e3ec69507883399
g47ben-lang <g47ben@gmail.com>
2026-05-12 17:30:32 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,30 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application") version "8.2.2"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
+}
+
 android {
     namespace = "com.example.shmuelkum"
     compileSdk = 34
-    defaultConfig { applicationId = "com.example.shmuelkum"; minSdk = 26; targetSdk = 34; versionCode = 4; versionName = "1.4" }
-    compileOptions { sourceCompatibility = JavaVersion.VERSION_1_8; targetCompatibility = JavaVersion.VERSION_1_8 }
-    kotlinOptions { jvmTarget = "1.8" }
+    
+    defaultConfig { 
+        applicationId = "com.example.shmuelkum"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 5
+        versionName = "1.5" 
+    }
+    
+    compileOptions { 
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8 
+    }
+    
+    kotlinOptions { 
+        jvmTarget = "1.8" 
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
